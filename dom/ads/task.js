126bd1762de9d380aedc54cfb66ff479a18cfc8e
Olga Rachkovskaj <volchek_o.k@mail.ru>
2023-03-13 17:54:46 +0300

    задание исправлено

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,23 +1,17 @@
-const rotatorArr = Array.from(document.querySelectorAll('.rotator__case'));
-let speed = 1000;
-let flag = true;
+let caseArr = Array.from(document.querySelectorAll('.rotator__case'));
 
-function rotator (arr){
-    if (flag) {
-        flag = false;
-        let activeIdx = arr.findIndex(item => item.classList.contains('rotator__case_active'));
-        arr[activeIdx].classList.remove('rotator__case_active');
-        if (activeIdx === arr.length - 1) {
-           activeIdx = 0;
-        } 
-        arr[activeIdx + 1].classList.add('rotator__case_active');
-        let color = arr[activeIdx + 1].dataset.color;
-        arr[activeIdx + 1].style.color = color;
-        speed = arr[activeIdx + 1].dataset.speed;
-        const delayResponse = setTimeout(() => {
-            flag = true;
-        }, speed);
-    }
+let rotate = function() {
+    let activeCaseIndex = caseArr.findIndex((elem, )=> {
+   return elem.classList.contains('rotator__case_active')
+});
+
+caseArr[activeCaseIndex].classList.remove('rotator__case_active');
+
+if (activeCaseIndex < (caseArr.length - 1)) {
+    caseArr[activeCaseIndex + 1].classList.add('rotator__case_active');
+} else {
+    caseArr[0].classList.add('rotator__case_active'); 
+} 
 }
-    
- setInterval ( rotator, 0, rotatorArr);
+
+let timer = setInterval(rotate,1000)
